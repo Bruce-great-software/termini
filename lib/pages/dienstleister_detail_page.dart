1dbd088f5503f1649c5e9b4b66f419cbc0d94d2c
Bruce-great-software <serdal.koese@googlemail.com>
2026-03-13 15:53:50 +0100

    Make category section bar full-width

--- lib/pages/dienstleister_detail_page.dart
+++ lib/pages/dienstleister_detail_page.dart
@@ -647,12 +647,9 @@ class _SectionBlock extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Container(
-          margin: const EdgeInsets.fromLTRB(16, 12, 16, 8),
-          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
-          decoration: BoxDecoration(
-            color: const Color(0xFF4E8DF5),
-            borderRadius: BorderRadius.circular(8),
-          ),
+          margin: const EdgeInsets.fromLTRB(0, 12, 0, 8),
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+          color: const Color(0xFF4E8DF5),
           child: Text(
             section.title,
             style: const TextStyle(
