e3324193816157dc6577add229ac7dc330697a7d
Serdal Köse <serdal.koese@googlemail.com>
2026-04-04 13:31:54 +0200

    Header Navi auf Partner Seite und zurück

--- lib/pages/alle_dienstleister_page.dart
+++ lib/pages/alle_dienstleister_page.dart
@@ -3880,12 +3880,27 @@ class _AlleDienstleisterPageState extends State<AlleDienstleisterPage>
       body: kIsWeb
           ? AlleDienstleisterWebShell(
         showHeader: geoeffneterDienstleister == null,
+        onLogoPressed: () {
+          setState(() {
+            geoeffneterDienstleister = null;
+            _dienstleisterFavorisiert = false;
+            _selectedIndex = 0;
+          });
+        },
         onMeinKontoPressed: _openProfilFromWebHeader,
         onPartnerWerdenPressed: () {
           Navigator.push(
             context,
             MaterialPageRoute(
               builder: (_) => PartnerWerdenPage(
+                onHeaderLogoPressed: () {
+                  Navigator.of(context).pop();
+                  setState(() {
+                    geoeffneterDienstleister = null;
+                    _dienstleisterFavorisiert = false;
+                    _selectedIndex = 0;
+                  });
+                },
                 onHeaderPartnerWerdenPressed: () {},
                 onHeaderMeinKontoPressed: () {
                   _openProfilFromWebHeader();
